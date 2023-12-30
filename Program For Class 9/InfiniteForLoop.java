bd0c374c78449e76a579adb5b31e668ca2745352
Sasanka Sekhar Kundu <sasankasekharkundu24@gmail.com>
2023-12-31 01:22:27 +0530

    Delete Program For Class 9 directory

--- Program For Class 9/InfiniteForLoop.java
+++
@@ -1,8 +0,0 @@
-public class InfiniteForLoop {
-    public static void main(String[] args) {
-        // Infinite for loop
-        for (;;) {
-            System.out.println("This is an infinite loop!");
-        }
-    }
-}
