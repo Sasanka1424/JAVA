37814abc48e5ed6b9600947fae4cf13803949f30
Sasanka <sasankasekharkundu24@gmail.com>
2024-05-20 12:07:26 +0530

    The Java program JavaBasicPrintPattern prints a pattern of asterisks (*) in decreasing order on consecutive lines. The pattern consists of four lines, each containing a decreasing number of asterisks from left to right

---
+++ Java Basic to Advance/JavaBasicPrintPattern.java
@@ -0,0 +1,14 @@
+/* Q1. Print Pattern 
+   ****
+   ***
+   **
+   *
+ */
+public class JavaBasicPrintPattern {
+    public static void main(String args[]){
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
