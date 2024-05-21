fd0a70843d4ae86cfe9b01c36c6d09a8ad2525ad
Sasanka <sasankasekharkundu24@gmail.com>
2024-05-21 20:57:44 +0530

    The difference between print and println in Java is that print outputs text to the console without adding a new line at the end, while println outputs text to the console and then adds a new line

---
+++ Java Basic to Advance/VariableAndDataTypes/JavaBasic2.java
@@ -0,0 +1,16 @@
+/* By using "println" and "/n" statement give output*/
+public class JavaBasic2 {
+    public static void main(String[] args) {
+        // Print on different lines using println (automatically adds a newline at the end)
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+
+        // Print on different lines using print (manually adding a newline character '\n')
+        System.out.print("Hello World!\n"); 
+        System.out.print("Hello World!\n"); 
+        System.out.print("Hello World!\n"); 
+    }
+}
+
+
