0f4ef05edb466e647a5d40812878df59410c624a
Sasanka <sasankasekharkundu24@gmail.com>
2024-05-24 13:07:51 +0530

    Summary of the Code:: The given Java code demonstrates the use of unary operators with two integer variables, a and b, both initialized to 10.

---
+++ Java Basic to Advance/Operaters/UnaryOperators .java
@@ -0,0 +1,17 @@
+/* Q1. What will be the output of the following Java code that demonstrates the use of unary operators 
+(post-increment, pre-increment, post-decrement, and pre-decrement)? */
+public class UnaryOperators {
+    // main function
+    public static void main(String[] args) {
+        // Integer declared
+        int a = 10;
+        int b = 10;
+
+        // Using unary operators
+        System.out.println("Postincrement : " + (a++));
+        System.out.println("Preincrement : " + (++a));
+
+        System.out.println("Postdecrement : " + (b--));
+        System.out.println("Predecrement : " + (--b));
+    }
+}
