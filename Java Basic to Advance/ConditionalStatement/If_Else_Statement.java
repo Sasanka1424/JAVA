43a1a004793882030bd6a487048c03ec5204ed9b
Sasanka <sasankasekharkundu24@gmail.com>
2024-05-26 14:18:39 +0530

    Summary of the code:: This Java code demonstrates the use of if-else statements to evaluate a condition and execute different blocks of code based on whether the condition is true or false. The code initializes an integer variable x with the value -10. It then checks if x is greater than 0. If the condition is true, it prints "x is positive"; otherwise, it prints "x is not positive". Since x is -10 (which is not greater than 0), the output of the code will be "x is not positive".

---
+++ Java Basic to Advance/ConditionalStatement/If_Else_Statement.java
@@ -0,0 +1,14 @@
+/* Q2.What will be the output of the following Java code that checks whether a variable x is positive or not using if-else statements? */
+public class If_Else_Statement {
+    public static void main(String[] args) {
+        // Define the value of x
+        int x = -10;
+
+        // Check if x is greater than 0
+        if (x > 0) {
+            System.out.println("x is positive");
+        } else {
+            System.out.println("x is not positive");
+        }
+    }
+}
