a898a4dd5687f7e1d5b39313f8c08f36fb99c41b
Sasanka <sasankasekharkundu24@gmail.com>
2024-05-27 00:35:02 +0530

    Summary of the code:: The provided code demonstrates the use of an if-else statement in Java to determine and print the larger of two numbers, A and B, or indicate if they are equal. The program initializes two integer variables, A and B, with the values 1 and 3, respectively. Using the if-else statement, it compares these two numbers and prints a message indicating whether A is larger than B or if they are equal.

---
+++ Java Basic to Advance/ConditionalStatement/ProblemQuestion1.java
@@ -0,0 +1,16 @@
+/* Q1. Print largest of 2 numbers using if-else Statement. 
+       A = 1;
+       B = 3;
+ */
+public class ProblemQuestion1 {
+    public static void main(String[] args) {
+        int A = 1;
+        int B = 3;
+
+        if (A > B) {
+            System.out.println("A is larger than B");
+        } else {
+            System.out.println("B is larger than A");
+        }
+    }
+}
