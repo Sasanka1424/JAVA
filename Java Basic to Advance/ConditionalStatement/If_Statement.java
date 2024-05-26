d28de54d284b4dd3888eecde41419743f1130a65
Sasanka <sasankasekharkundu24@gmail.com>
2024-05-26 13:55:11 +0530

    Summary of the code:: This Java code demonstrates the use of an if statement to evaluate a condition. The code initializes an integer variable x with the value 10. It then uses an if statement to check if x is greater than 0. If the condition is true, it prints "x is positive" to the console. Since x is indeed greater than 0, the output of the code will be "x is positive".

---
+++ Java Basic to Advance/ConditionalStatement/If_Statement.java
@@ -0,0 +1,11 @@
+/* Q1. Q1. What will be the output of the following Java code that uses an if statement to check if a variable x is greater than 0? */
+public class If_Statement {
+    public static void main(String[] args) {
+        int x = 10;
+
+        // Check if x is greater than 0
+        if (x > 0) {
+            System.out.println("x is positive"); // Print a message if x is positive
+        }
+    }
+}
