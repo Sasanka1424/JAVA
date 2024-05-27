5f839573f387e0cd1bfc5765daa61710a2b29613
Sasanka <sasankasekharkundu24@gmail.com>
2024-05-27 20:41:24 +0530

    Summary of the code:: The provided code prints numbers from 1 to 10 using a while loop. The loop starts with i initialized to 1 and continues to run as long as i is less than or equal to 10. On each iteration, the current value of i is printed, and then i is incremented by 1.

---
+++ Java Basic to Advance/Loops(FlowControl)/ProblemQuestion1.java
@@ -0,0 +1,23 @@
+/* Q1. Print numbers from 1 to 10.
+   Format of printing in console is:
+      1
+      2
+      3
+      4
+      5
+      6
+      7
+      8
+      9
+      10
+ */
+
+ public class ProblemQuestion1 {
+    public static void main(String[] args) {
+        int i = 1; // Initialize the counter variable
+        while (i <= 10) { // Condition to run the loop
+            System.out.println(i); // Print the current value of i
+            i++; // Increment the counter variable
+        }
+    }
+}
